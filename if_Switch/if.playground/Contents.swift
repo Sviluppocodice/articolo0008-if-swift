50418b45907fe69570089269c91a474c4a7e3ef5
SviluppoCodice <51750512+Sviluppocodice@users.noreply.github.com>
2019-07-22 15:30:11 +0200

    Add files via upload

---
+++ if_Switch/if.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var a = 100
+
+if a < 10 {
+    
+    print("a è minore di 10")
+}
+else if a >= 10 && a <= 20 {
+    
+    print("a è maggiore o uguale a 10 e nimore o unguale a 20")
+    print("altro codice sotto")
+}
+else{
+    
+    print("a è maggiore di venti")
+}
